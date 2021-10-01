ba3c7a8240b2013784d4af68d7ea5377985231e1
Christian Hågenvik <55736321+chagenvik@users.noreply.github.com>
2021-10-01 08:51:56 +0200

    update

--- folderstructure/project2/main.py
+++ folderstructure/project2/main.py
@@ -6,8 +6,12 @@ Created on Mon Mar  1 07:29:41 2021
 """
 
 import sys
+import os
+
+#os.chdir('../common/')   #changing cwd to the common folder will solve the problem
 
 sys.path.append('..')
+
 from common.func import *
 
 print(myfunc(5,3))
