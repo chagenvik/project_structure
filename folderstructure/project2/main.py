5bacd1aceccb4644fc54592bd9027a6165729877
Christian Hågenvik <55736321+chagenvik@users.noreply.github.com>
2021-10-01 09:03:02 +0200

    update

--- folderstructure/project2/main.py
+++ folderstructure/project2/main.py
@@ -7,11 +7,16 @@ Created on Mon Mar  1 07:29:41 2021
 
 import sys
 import os
+import pandas as pd
 
-#os.chdir('../common/')   #changing cwd to the common folder will solve the problem
+os.chdir('../common/')   #changing cwd to the common folder will solve the problem
 
 sys.path.append('..')
 
 from common.func import *
+
+os.chdir('../project2/')    #changing cwd back to project2 folder
+#read project2spesific data
+y=pd.read_csv('project2spesificdata.csv')
 
 print(myfunc(5,3))
